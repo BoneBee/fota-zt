79806e289280712547973cdd6583faddf4f9f274
zhanghui <zhanghui@intest.cn>
2020-08-25 20:21:04 +0800

    增加日志配置文件

--- api/src/main/java/com/intest/api/controller/TestController.java
+++ api/src/main/java/com/intest/api/controller/TestController.java
@@ -5,11 +5,12 @@ import com.intest.service.CarService;
 import com.intest.service.impl.CarPage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.logging.Logger;
 
 /**
  * @author zhanghui
@@ -19,7 +20,7 @@ import java.util.logging.Logger;
 @Api(tags = "测试")
 public class TestController {
 
-    //private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private CarService carService;
@@ -27,6 +28,7 @@ public class TestController {
     @GetMapping("/api/test")
     @ApiOperation(value = "获取车辆")
     public PagerDataBaseVO test() {
+        //logger.error("测试");
         return carService.getCarInfo(new CarPage());
     }
 
